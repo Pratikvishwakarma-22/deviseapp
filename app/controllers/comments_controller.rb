221c0af661d0fdbf9f7c3b29ef3ea36ad88c6876
Pratik_vish <Pratikvishwa22@gmail.com>
2023-03-31 16:13:10 +0530

    feat: CRUD operations on articles and on associate comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params.merge(article_id: params[:article_id]))
+    @article = Article.find(params[:article_id])
+
+    if @comment.save
+
+      redirect_to @article
+    else
+
+      redirect_to article_path(@article, comment_show_params)
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to article_path(@comment.article), status: :see_other
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment, :article_id)
+  end
+
+  def comment_show_params
+    {
+      comment: @comment.comment,
+      comment_err: @comment.errors.full_messages_for(:comment).first,
+      commenter_err: @comment.errors.full_messages_for(:commenter).first
+    }
+  end
+end
