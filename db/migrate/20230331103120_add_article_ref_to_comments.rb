46a6749747164ae33ef117530d432c5e365383bf
Pratik_vish <Pratikvishwa22@gmail.com>
2023-04-06 15:30:05 +0530

    feat: authorization

--- db/migrate/20230331103120_add_article_ref_to_comments.rb
+++ db/migrate/20230331103120_add_article_ref_to_comments.rb
@@ -3,3 +3,4 @@ class AddArticleRefToComments < ActiveRecord::Migration[7.0]
     add_reference :comments, :article, null: false, foreign_key: true
   end
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_05_090951) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_134428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_090951) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -40,9 +42,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_090951) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
